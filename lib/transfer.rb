b6d04f3973225bed723f768d6a483ca55e74bee6
Mike Cooper <mikescooper@icloud.com>
2019-09-16 07:14:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,9 +20,13 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     else 
-      "Transaction rejected. Please check your account balance."
-      @status = "rejected"
+      reject_transaction
     end 
+  end 
+  
+  def reject_transaction
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
   end 
   
 end
